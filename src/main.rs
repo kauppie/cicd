251cef69bb024bbfa329366de10fc9b02b7378ee
Elias Kauppi <elkku.kauppi@gmail.com>
2022-05-31 22:23:04 +0300

    Use prettier typing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::error::Error as StdError;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn StdError>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
     loop {
